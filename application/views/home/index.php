c4ed49d4e2ab90d6070dc1661473a1c4a56c248e
Kenji Prahyudi Mudjono <kenji_prahyudi@yahoo.com>
2019-08-10 22:45:11 +0700

    Update index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -13,6 +13,6 @@
 </div>
 <div class="col-12 col-md-8 col-xl-9">
 	<h3 id="guitar_tab_title"></h3>
-	<div id="guitar_tab_content" class="text-nowrap text-monospace"></div>
 	<div id="guitar_tab_video_embed_html"></div>
+	<div id="guitar_tab_content" class="text-nowrap text-monospace"></div>
 </div>
