9b900dc658ffb73a147619ccb2d852fe0fea5985
Kenji Prahyudi Mudjono <kenji_prahyudi@yahoo.com>
2019-08-10 22:01:55 +0700

    update portfolio

--- application/views/portfolio/index.php
+++ application/views/portfolio/index.php
@@ -1,42 +1,62 @@
 <div class="col-12">
 	<div class="container-fluid">
-		<h3>Web Development</h3>
-		<div class="row">
-			<div class="card col-12 col-md-6 col-lg-4">
-				<img src="/img/portfolio/dams.png" class="card-img-top" alt="DAMS Minister of Finance">
+		<h3>Business Ownership</h3>
+		<div class="card-deck">
+			<div class="card">
+				<img src="/img/portfolio/bjcoffee.png" class="card-img-top" alt="BlackJack Coffee">
+				<h5 class="card-title">BlackJack Coffee</h5>
 				<div class="card-body">
-					<p class="card-text">DAMS (Daily Activity Monitoring Service) is a system for monitoring all activities (meetings, events, etc) for Minister of Finance in Indonesia.</p>
+					<p class="card-text">BlackJack Coffee is a coffee shop established in 2013. Still operating until now.</p>
 				</div>
 			</div>
-			<div class="card col-12 col-md-6 col-lg-4">
-				<img src="/img/portfolio/ir.png" class="card-img-top" alt="IR Star Energy">
+			<div class="card">
+				<img src="/img/portfolio/bjvape.png" class="card-img-top" alt="BlackJack Vaporizer">
+				<h5 class="card-title">BlackJack Vaporizer</h5>
 				<div class="card-body">
-					<p class="card-text">IR (Incident Reporting) is a system for safety management for Star Energy Salak Darajat, Geothermal Company in Indonesia.</p>
-				</div>
-			</div>
-			<div class="card col-12 col-md-6 col-lg-4">
-				<img src="/img/portfolio/pertagas.png" class="card-img-top" alt="IS Pertagas">
-				<div class="card-body">
-					<p class="card-text">Information System for managing all organization's operation for Pertagas, Gas Company in Indonesia</p>
-				</div>
-			</div>
-			<div class="card col-12 col-md-6 col-lg-4">
-				<img src="/img/portfolio/tsis.png" class="card-img-top" alt="TSIS Pertamina">
-				<div class="card-body">
-					<p class="card-text">Project Investment Management System for Pertamina, Oil Company in Indonesia</p>
-				</div>
-			</div>
-			<div class="card col-12 col-md-6 col-lg-4">
-				<img src="/img/portfolio/this.png" class="card-img-top" alt="This Website">
-				<div class="card-body">
-					<p class="card-text">And yes, of course, this is a website I built. First released after 12 work hours.</p>
+					<p class="card-text">BlackJack Vaporizer is a vaporizer store established in 2014. Still operating until now.</p>
 				</div>
 			</div>
 		</div>
 		<br/>
-		<h3>Business Owner</h3>
-		<div>
-			
+		<h3>Web Development</h3>
+		<div class="card-deck">
+			<div class="card">
+				<img src="/img/portfolio/dams.png" class="card-img-top" alt="DAMS Minister of Finance">
+				<h5 class="card-title">DAMS - Minister of Finance</h5>
+				<div class="card-body">
+					<p class="card-text">DAMS (Daily Activity Monitoring Service) is a system for monitoring all activities (meetings, events, etc) for Minister of Finance in Indonesia.</p>
+				</div>
+			</div>
+			<div class="card">
+				<img src="/img/portfolio/ir.png" class="card-img-top" alt="IR Star Energy">
+				<h5 class="card-title">IR - Star Energy</h5>
+				<div class="card-body">
+					<p class="card-text">IR (Incident Reporting) is a system for safety management for Star Energy Salak Darajat, Geothermal Company in Indonesia.</p>
+				</div>
+			</div>
+			<div class="card">
+				<img src="/img/portfolio/pertagas.png" class="card-img-top" alt="IS Pertagas">
+				<h5 class="card-title">IS - Pertagas</h5>
+				<div class="card-body">
+					<p class="card-text">Information System for managing all organization's operation for Pertagas, Gas Company in Indonesia</p>
+				</div>
+			</div>
+		</div>
+		<div class="card-deck">
+			<div class="card">
+				<img src="/img/portfolio/tsis.png" class="card-img-top" alt="TSIS Pertamina">
+				<h5 class="card-title">TSIS - Pertamina</h5>
+				<div class="card-body">
+					<p class="card-text">Project Investment Management System for Pertamina, Oil Company in Indonesia</p>
+				</div>
+			</div>
+			<div class="card">
+				<img src="/img/portfolio/this.png" class="card-img-top" alt="This Website">
+				<h5 class="card-title">This Website</h5>
+				<div class="card-body">
+					<p class="card-text">And yes, of course, this is a website I built. First released after 12 work hours.</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
