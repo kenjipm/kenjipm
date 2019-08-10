ab35f018f8b05f13e625a388f815b92f11fad2ef
Kenji Prahyudi Mudjono <kenji_prahyudi@yahoo.com>
2019-08-10 21:42:28 +0700

    portfolio update

--- application/views/portfolio/index.php
+++ application/views/portfolio/index.php
@@ -3,12 +3,37 @@
 		<h3>Web Development</h3>
 		<div class="row">
 			<div class="card col-12 col-md-6 col-lg-4">
-				<img src="/img/portfolio/" class="card-img-top" alt="...">
+				<img src="/img/portfolio/dams.png" class="card-img-top" alt="DAMS Minister of Finance">
 				<div class="card-body">
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<p class="card-text">DAMS (Daily Activity Monitoring Service) is a system for monitoring all activities (meetings, events, etc) for Minister of Finance in Indonesia.</p>
+				</div>
+			</div>
+			<div class="card col-12 col-md-6 col-lg-4">
+				<img src="/img/portfolio/ir.png" class="card-img-top" alt="IR Star Energy">
+				<div class="card-body">
+					<p class="card-text">IR (Incident Reporting) is a system for safety management for Star Energy Salak Darajat, Geothermal Company in Indonesia.</p>
+				</div>
+			</div>
+			<div class="card col-12 col-md-6 col-lg-4">
+				<img src="/img/portfolio/pertagas.png" class="card-img-top" alt="IS Pertagas">
+				<div class="card-body">
+					<p class="card-text">Information System for managing all organization's operation for Pertagas, Gas Company in Indonesia</p>
+				</div>
+			</div>
+			<div class="card col-12 col-md-6 col-lg-4">
+				<img src="/img/portfolio/tsis.png" class="card-img-top" alt="TSIS Pertamina">
+				<div class="card-body">
+					<p class="card-text">Project Investment Management System for Pertamina, Oil Company in Indonesia</p>
+				</div>
+			</div>
+			<div class="card col-12 col-md-6 col-lg-4">
+				<img src="/img/portfolio/this.png" class="card-img-top" alt="This Website">
+				<div class="card-body">
+					<p class="card-text">And yes, of course, this is a website I built. First released after 12 work hours.</p>
 				</div>
 			</div>
 		</div>
+		<br/>
 		<h3>Business Owner</h3>
 		<div>
 			
