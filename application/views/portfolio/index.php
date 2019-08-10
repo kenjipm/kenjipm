14e41057df984e22b8a7e27d694db68d7b9df462
Kenji Prahyudi Mudjono <kenji_prahyudi@yahoo.com>
2019-08-10 22:18:58 +0700

    update portfolio layout

--- application/views/portfolio/index.php
+++ application/views/portfolio/index.php
@@ -3,14 +3,14 @@
 		<h3>Business Ownership</h3>
 		<div class="card-deck">
 			<div class="card">
-				<img src="/img/portfolio/bjcoffee.png" class="card-img-top" alt="BlackJack Coffee">
+				<img src="/img/portfolio/bjcoffee.png" class="card-img-top kpm-card-img" alt="BlackJack Coffee">
 				<div class="card-body">
 					<h5 class="card-title">BlackJack Coffee</h5>
 					<p class="card-text">BlackJack Coffee is a coffee shop established in 2013. Still operating until now.</p>
 				</div>
 			</div>
 			<div class="card">
-				<img src="/img/portfolio/bjvape.png" class="card-img-top" alt="BlackJack Vaporizer">
+				<img src="/img/portfolio/bjvape.png" class="card-img-top kpm-card-img" alt="BlackJack Vaporizer">
 				<div class="card-body">
 					<h5 class="card-title">BlackJack Vaporizer</h5>
 					<p class="card-text">BlackJack Vaporizer is a vaporizer store established in 2014. Still operating until now.</p>
@@ -23,21 +23,21 @@
 		<h3>Web Development</h3>
 		<div class="card-deck">
 			<div class="card">
-				<img src="/img/portfolio/dams.png" class="card-img-top" alt="DAMS Minister of Finance">
+				<img src="/img/portfolio/dams.png" class="card-img-top kpm-card-img" alt="DAMS Minister of Finance">
 				<div class="card-body">
 					<h5 class="card-title">DAMS - Minister of Finance</h5>
 					<p class="card-text">DAMS (Daily Activity Monitoring Service) is a system for monitoring all activities (meetings, events, etc) for Minister of Finance in Indonesia.</p>
 				</div>
 			</div>
 			<div class="card">
-				<img src="/img/portfolio/ir.png" class="card-img-top" alt="IR Star Energy">
+				<img src="/img/portfolio/ir.png" class="card-img-top kpm-card-img" alt="IR Star Energy">
 				<div class="card-body">
 					<h5 class="card-title">IR - Star Energy</h5>
 					<p class="card-text">IR (Incident Reporting) is a system for safety management for Star Energy Salak Darajat, Geothermal Company in Indonesia.</p>
 				</div>
 			</div>
 			<div class="card">
-				<img src="/img/portfolio/pertagas.png" class="card-img-top" alt="IS Pertagas">
+				<img src="/img/portfolio/pertagas.png" class="card-img-top kpm-card-img" alt="IS Pertagas">
 				<div class="card-body">
 					<h5 class="card-title">IS - Pertagas</h5>
 					<p class="card-text">Information System for managing all organization's operation for Pertagas, Gas Company in Indonesia</p>
@@ -46,14 +46,14 @@
 		</div>
 		<div class="card-deck">
 			<div class="card">
-				<img src="/img/portfolio/tsis.png" class="card-img-top" alt="TSIS Pertamina">
+				<img src="/img/portfolio/tsis.png" class="card-img-top kpm-card-img" alt="TSIS Pertamina">
 				<div class="card-body">
 					<h5 class="card-title">TSIS - Pertamina</h5>
 					<p class="card-text">Project Investment Management System for Pertamina, Oil Company in Indonesia</p>
 				</div>
 			</div>
 			<div class="card">
-				<img src="/img/portfolio/this.png" class="card-img-top" alt="This Website">
+				<img src="/img/portfolio/this.png" class="card-img-top kpm-card-img" alt="This Website">
 				<div class="card-body">
 					<h5 class="card-title">This Website</h5>
 					<p class="card-text">And yes, of course, this is a website I built. First released after 12 work hours.</p>
