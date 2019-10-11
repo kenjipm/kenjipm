04a9a7d43105e863dd58a7d3eccb93dbd7e0989d
Kenji Prahyudi Mudjono <kenji_prahyudi@yahoo.com>
2019-10-12 00:24:16 +0700

    Update serviceworker.js

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -11,7 +11,7 @@ if ('serviceWorker' in navigator) {
 
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', function(event) {
+window.addEventListener('beforeinstallprompt', function(e) {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
